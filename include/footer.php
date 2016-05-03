35d5921717f0b36ba18058c01f4ba377d930b3ba
Ayush Bhatnagar <ayushbhatnagar@Ayushs-MacBook-Pro.local>
2016-05-03 16:57:26 +0530

    CDN for olivine solutions

---
+++ include/footer.php
@@ -0,0 +1,62 @@
+<!-- news letter -->
+		<div class="subscribe text-center">
+			<div class="container">
+				<h3>Subscribe to Our Newsletter</h3>
+				<p>Subscribe to our weekly Newsletter and stay tuned to all our updates..</p>
+				<form>
+					<input placeholder="Email Address" class="user" type="text" required>
+					<input type="submit" value="Subscribe">
+				</form>
+				<p class="spam">We never share your information or use it to spam you</p>
+			</div>
+		</div>
+		<!-- //news letter -->
+		
+
+<footer>
+			<div class="footer-grids">
+				<div class="container">
+					<div class="col-md-3 footer one">
+						<h3>About Company</h3>
+						<p> WE ARE PASSIONATE ABOUT GROWTH. Olivine Solutions, primarily is a service provider and business promoter. In addition to the core business, the company is also involved in Outsourcing, Contractual Projects and Research and Analysis.</p>
+						<p class="adam">- Om Swarup Bindhani</p>
+						<div class="clear"></div>
+					</div>
+					
+					<div class="col-md-3 footer two">
+						<h3>Keep Connected</h3>
+						<ul>
+							<li><a class="fb" href="https://www.facebook.com/OlivineSolutions?fref=ts"><i class="fa fa-facebook"></i>Like us on Facebook</a></li>
+							<li><a class="fb1" href="https://twitter.com/OlivineSolution"><i class="fa fa-twitter"></i>Follow us on Twitter</a></li>
+							<li><a class="fb2" href="#"><i class="fa fa-google-plus"></i>Add us on Google Plus</a></li>
+							<li><a class="fb3" href="https://www.linkedin.com/company/2557704?trk=tyah&trkInfo=clickedVertical%3Acompany%2Cidx%3A2-1-7%2CtarId%3A1432001554609%2Ctas%3Aolivine%20s"><i class="fa fa-linkedin"></i>Connect with us on Linkedin</a></li>
+							
+						</ul>
+					</div>
+					<div class="col-md-3 footer three">
+						<h3>Contact Information</h3>
+						<ul>
+							<li><i class="fa fa-map-marker"></i><p>Olivine Solutions <span> Mayur Vihar Phase-II,</span>                        New Delhi-110091. </p><div class="clearfix"></div> </li>
+							<li><i class="fa fa-phone"></i><p> +91-95555-88850</p> <div class="clearfix"></div> </li>
+							<li><i class="fa fa-envelope-o"></i><a href="mailto:info@olivinesolutions.com">info@olivinesolutions.com</a> <div class="clearfix"></div></li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<!-- maps -->
+			<div id="map">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6240.435715448571!2d77.30087509925546!3d28.619986225381098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce4b0800fbbcd%3A0xfccd170b06156896!2sPocket-A!5e0!3m2!1sen!2sin!4v1439537085624" frameborder="0" style="border:0"></iframe>
+			</div>
+			<div class="copy-right-grids">
+				<div class="container">
+					<div class="copy-left">
+							<p class="footer-gd">© 2015 Olivine Solutions. All Rights Reserved | Developed by <a href="http://codeai.net/" target="new">CodeAI</a></p>
+					</div>
+					<div class="footer-links">
+						
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</footer>
